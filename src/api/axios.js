24acde6bc0515c21066d950210bdc99fe287f988
umairansari92 <umair.ansari.92@gmail.com>
2026-02-06 02:04:27 +0500

    Updated backend URL in axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://c-vify-backend.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
