5f28143dd461900602dde4bb26211e8daf295279
umairansari92 <umair.ansari.92@gmail.com>
2026-03-08 02:34:53 +0500

    update live profile into portfolio builder

--- src/pages/PublicProfile.jsx
+++ src/pages/PublicProfile.jsx
@@ -14,6 +14,7 @@ import {
   FaMapMarkerAlt,
   FaBriefcase,
   FaGraduationCap,
+  FaTimes,
 } from "react-icons/fa";
 import { motion, AnimatePresence } from "framer-motion";
 import { toast } from "react-hot-toast";
